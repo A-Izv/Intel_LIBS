eec72e8c7a71dd893702f63c7bb983a4a374ffe0
Artyom <Artyom@I.co>
2021-01-02 22:35:10 +0300

    Thread-safe exceptions in ippCustom

--- IPP/custom/ippCustom.cpp
+++ IPP/custom/ippCustom.cpp
@@ -1,6 +1,67 @@
+//------------------------------------------------------------------------------
+#include <QDebug>
 #include "ippCustom.h"
 //------------------------------------------------------------------------------
-const QString IPP_CUSTOM_MSG     = QStringLiteral("В файле '%1' в строке №%2 произошла %3");
-const QString IPP_CUSTOM_IPP     = QStringLiteral("ошибка IPP : '%1'.");
-const QString IPP_CUSTOM_UNKNOWN = QStringLiteral("неизвестная ошибка.");
+#ifdef IPP_CHK_WITH_EXCEPTIONS
+    #define IPP_CHK_ERR_REACTION    \
+        QtIppChkException e(m); /*/создаем объект исключения/*/\
+        e.raise();              /*/вызываем исключение/*/
+#else
+    #define IPP_CHK_ERR_REACTION /*/пустышка/*/
+#endif
+//------------------------------------------------------------------------------
+#include <mutex>
+//------------------------------------------------------------------------------
+std::string CHKlastMessage;     // сохраняемое сообщение об ошибке
+std::mutex  CHKMutex;           // мьютекс для корректной многопоточной обработки
+//------------------------------------------------------------------------------
+bool ippChkIsOk() {
+    CHKMutex.lock();
+    bool result = CHKlastMessage.empty();
+    CHKMutex.unlock();
+
+    return result;
+}
+//------------------------------------------------------------------------------
+std::string ippChkLastMessage() {
+    CHKMutex.lock();
+    std::string result = CHKlastMessage;
+    CHKMutex.unlock();
+
+    return result;
+}
+//------------------------------------------------------------------------------
+void ippChkPrepare()
+{
+    CHKMutex.lock();
+    CHKlastMessage.clear();
+    CHKMutex.unlock();
+}
+//------------------------------------------------------------------------------
+void ippChkDoOnError( IppStatus stts, const char *file, int line )
+{
+ // формируем сообщение
+    std::string m;
+    m += "In file '";
+    m += file;
+    m += "' on line ";
+    m += std::to_string(line);
+    m += " occured an ";
+    if( stts != IppStatus(IPP_CUSTOM_ERR) ) {
+        m += "IPP error: '";
+        m += ippGetStatusString(stts);
+        m += "'";
+    } else {
+        m += "unknown error.";
+    }
+
+ // запоминаем сообщение
+    CHKMutex.lock();
+    CHKlastMessage = m;
+    CHKMutex.unlock();
+ // выводим в поток отладки
+    qDebug() << m.c_str();
+ // (возможно) вызываем исключение
+    IPP_CHK_ERR_REACTION;
+}
 //------------------------------------------------------------------------------
--- IPP/custom/ippCustom.h
+++ IPP/custom/ippCustom.h
@@ -1,48 +1,90 @@
 #ifndef IPPCUSTOM_H
 #define IPPCUSTOM_H
 //------------------------------------------------------------------------------
-#include <QDebug>
+//  В файле описан макрос для проверки результатов работы функций IPP,
+//  кроме того подключаются заголовочные файлы ippcore.h, ipps.h и ippvm.h
+//
+//  Главным определением является макрос CHK().
+//  Обернутые им функции IPP в случае ошибки выводят в поток отладки подробные сообщения о неполадках,
+//  а также имя файла и номер строки, содержащие код вызвавший ошибку.
+//
+//  Если в свойствах проекта определено IPP_CHK_WITH_EXCEPTIONS, то кроме вывода сообщения будет
+//  вызвано исключение.
+//      для qmake : DEFINES += IPP_CHK_WITH_EXCEPTIONS
+//
+//  Если в свойствах проекта определено NO_IPP_DEBUG, то никакой обработки ошибок не будет
+//
+//  ПРИМЕР:
+//      try {
+//          ...
+//          CHK( ippsCopy_8u(...) );
+//          ..
+//      }
+//      catch( const QException &e ) {
+//          DoOnError( e.what() );
+//      }
+//      catch(...) {
+//          DoOnError();
+//      }
+//  Текст последней ошибки можно получить с помощью функции
+//      ippChkLastMessage();
+//  Закончилась ли последний вызов ошибкой можно проверить с помощью функции
+//      ippChkIsOk();
 //------------------------------------------------------------------------------
-// подключаем заголовочные файлы из IPP
-#include <ippcore.h>
-#include <ipps.h>
-#include <ippvm.h>
-//------------------------------------------------------------------------------
-#define IPP_CUSTOM_ERR 0x7FFFFFFF // константа для обозначения ошибки не-IPP
-//------------------------------------------------------------------------------
-// чтобы не плодить строки - объявляем строковые константы
-extern const QString IPP_CUSTOM_MSG;
-extern const QString IPP_CUSTOM_IPP;
-extern const QString IPP_CUSTOM_UNKNOWN;
-//------------------------------------------------------------------------------
-#ifdef IPP_CHK_WITH_EXCEPTIONS
-    #define IPP_CHK_ERR_REACTION    throw(errStr);
+#ifdef NO_IPP_DEBUG
+    #define CHK( x ) x;
 #else
-    #define IPP_CHK_ERR_REACTION /*/пустышка/*/
-#endif
+    //--------------------------------------------------------------------------
+    // функции для проверки последней ошибки
+    #include <string>
+    std::string ippChkLastMessage();
+    bool        ippChkIsOk();
+    //--------------------------------------------------------------------------
+    #ifdef IPP_CHK_WITH_EXCEPTIONS
+        #include <QException>
+        // Определение класса исключения для корректной передачи исключений
+        // между потоками в QtConcurrent.
+        class QtIppChkException : public QException {
+        private:
+            std::string message;
+        public:
+                        QtIppChkException( const QtIppChkException &e ) { this->message = e.message;       }
+                        QtIppChkException( const QString &m )           { this->message = m.toStdString(); }
+                        QtIppChkException( const std::string &m )       { this->message = m;               }
+                        QtIppChkException( const char *m )              { this->message = std::string(m);  }
+
+            const char* what()  const noexcept override { return message.c_str();              }    // std::exception interface
+            void        raise() const          override { throw *this ;                        }    // QException interface
+            QException* clone() const          override { return new QtIppChkException(*this); }    // QException interface
+        };
+    #endif // IPP_CHK_WITH_EXCEPTIONS
+    //--------------------------------------------------------------------------
+    // подключаем заголовочные файлы из IPP
+    #include <ippcore.h>
+    #include <ipps.h>
+    #include <ippvm.h>
+    //--------------------------------------------------------------------------
+    // служебные функции макроса
+    void ippChkPrepare();
+    void ippChkDoOnError( IppStatus stts, const char *file, int line );
+    //--------------------------------------------------------------------------
+    #define IPP_CUSTOM_ERR 0x7FFFFFFF // константа для обозначения ошибки не-IPP
+    //--------------------------------------------------------------------------
+    #define CHK( x )\
+    {\
+        IppStatus __stts;\
+        ippChkPrepare();\
+        try {\
+            __stts = x;   /*выполняем переданное выражение и сохраняем результат*/\
+        }\
+        catch(...) {\
+            __stts = IppStatus(IPP_CUSTOM_ERR);    /*произошла неизвестная ошибка*/\
+        }\
+        if(Q_UNLIKELY( __stts != ippStsNoErr )) {\
+            ippChkDoOnError( __stts, __FILE__, __LINE__ ); /*формирование сообщения об ошибке*/\
+        }\
+    }
+    //--------------------------------------------------------------------------
+#endif  //NO_IPP_DEBUG
 //------------------------------------------------------------------------------
-#define CHK( x )\
-{\
-    IppStatus __stts;\
-    try {\
-        __stts = x;   /*выполняем переданное выражение и сохраняем результат*/\
-    }\
-    catch(...) {\
-        __stts = IppStatus(IPP_CUSTOM_ERR);    /*произошла неизвестная ошибка*/\
-    }\
-    if(Q_UNLIKELY( __stts != ippStsNoErr )) {\
-        /*формируем сообщение по шаблону*/\
-        QString errStr = IPP_CUSTOM_MSG.\
-                             arg(__FILE__).     /*подставляем имя файла*/\
-                             arg(__LINE__).     /*подставляем номер строки*/\
-                             arg(__stts != IppStatus(IPP_CUSTOM_ERR) ?\
-                                           IPP_CUSTOM_IPP.arg(ippGetStatusString(__stts))\
-                                           :\
-                                           IPP_CUSTOM_UNKNOWN\
-                             );\
-        qCritical() << errStr;\
-        IPP_CHK_ERR_REACTION;\
-    }\
-}
-//------------------------------------------------------------------------------
-#endif
+#endif  //IPPCUSTOM_H
